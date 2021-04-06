d56ceb5b826cdd82d725976ff7062ce904144ad4
way1234zzZ <2979523100@qq.com>
2021-04-06 14:22:43 +0800

    修改dfs写法

--- src/components/Cascader.vue
+++ src/components/Cascader.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="cascader" v-click-outside="close">
     <div class="trigger" @click="toggle">
-      {{result || '请选择'}}
+      {{ result || "请选择" }}
     </div>
     <div class="popOver" v-if="popoverVisible">
-      <g-cascader-item :loadingItem="loadingItem" :items="source" :height="popoverHeight" :selected="selected" @update:selected="onUpdateSelected" :loadData="loadData"></g-cascader-item>
+      <g-cascader-item
+        :loadingItem="loadingItem"
+        :items="source"
+        :height="popoverHeight"
+        :selected="selected"
+        @update:selected="onUpdateSelected"
+        :loadData="loadData"
+      >
+      </g-cascader-item>
     </div>
   </div>
 </template>
 <script>
-import gCascaderItem from './cascader-item'
-import ClickOutside from '@/click-outside.js'
+import gCascaderItem from "./cascader-item";
+import ClickOutside from "@/click-outside.js";
 export default {
-  name: 'gCascader',
+  name: "gCascader",
   directives: {
-    ClickOutside
+    ClickOutside,
   },
   props: {
     source: {
-      type: Array
+      type: Array,
     },
     popoverHeight: {
       Type: String,
@@ -26,89 +34,74 @@ export default {
     selected: {
       type: Array,
       default: () => {
-        return []
-      }
+        return [];
+      },
     },
     loadData: {
-      type: Function
-    }
+      type: Function,
+    },
   },
   data() {
     return {
       popoverVisible: false,
-      loadingItem: {}
-    }
+      loadingItem: {},
+    };
   },
   computed: {
     result() {
-      return this.selected.map((item) => item.name).join('/')
-    }
+      return this.selected.map((item) => item.name).join("/");
+    },
   },
   components: {
-    gCascaderItem
+    gCascaderItem,
   },
   methods: {
     onUpdateSelected(newSelected) {
-      this.$emit('update:selected', newSelected)
-      let lastItem = newSelected[newSelected.length - 1]
-      let simplest = (children, id) => {
-        return children.filter(item => item.id === id)[0]
-      }
-      let complex = (children, id) => {
-        let noChildren = []
-        let hasChildren = []
-        children.forEach(item => {
-          if (item.children) {
-            hasChildren.push(item)
-          } else {
-            noChildren.push(item)
+      this.$emit("update:selected", newSelected);
+      let lastItem = newSelected[newSelected.length - 1];
+      let dfs = (arr, id) => {
+        if (!arr) {
+          return;
+        }
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i].id === id) {
+            return arr[i];
           }
-        })
-        let found = simplest(noChildren, id)
-        if (found) {
-          return found
-        } else {
-          found = simplest(hasChildren, id)
-          if (found) { return found }
-          else {
-            for (let i = 0; i < hasChildren.length; i++) {
-              found = complex(hasChildren[i].children, id)
-              if (found) {
-                return found
-              }
-            }
-            return undefined
+          if (dfs(arr[i].children, id)) {
+            return dfs(arr[i].children, id);
           }
         }
-      }
+        return null;
+      };
       let updateSource = (result) => {
-        this.loadingItem = {}
-        let copy = JSON.parse(JSON.stringify(this.source))
-        let toUpdate = complex(copy, lastItem.id)
-        toUpdate.children = result
-        this.$emit('update:source', copy)
-      }
+        this.loadingItem = {};
+        let copy = JSON.parse(JSON.stringify(this.source));
+        console.log(copy);
+        let toUpdate = dfs(copy, lastItem.id);
+        toUpdate.children = result;
+        this.$emit("update:source", copy);
+      };
       if (!lastItem.isLeaf && this.loadData) {
-        this.loadData(lastItem, updateSource) // 回调:把别人传给我的函数调用一下
+        this.loadData(lastItem, updateSource); // 回调:把别人传给我的函数调用一下
         // 调回调的时候传一个函数,这个函数理论应该被调用
-        this.loadingItem = lastItem
+        this.loadingItem = lastItem;
       }
     },
     toggle() {
       if (this.popoverVisible === true) {
-        this.close()
+        this.close();
       } else {
-        this.open()
+        this.open();
       }
     },
     open() {
-      this.popoverVisible = true
+      this.popoverVisible = true;
     },
     close() {
-      this.popoverVisible = false
+      this.popoverVisible = false;
     },
-  }
-}
+  },
+};
 </script>
 <style lang="scss" scoped>
 .cascader {
